4ae244e4360a7050b50f864c20b5df51ff95abdc
abdulfattah alkasaji <abdulfattah.alkasaji@gmail.com>
2020-12-09 20:55:04 +0200

    hello

--- server.js
+++ server.js
@@ -30,12 +30,18 @@ app.use(bodyParser.json({ limit: '2mb' }));
 app.use(cors());
 
 // serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('client/build'));
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//     });
+// }
+
+// temp
+app.use(express.static('client/build'));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 // routes middleware
 readdirSync('./routes').map((r) => app.use('/api', require('./routes/' + r)));
